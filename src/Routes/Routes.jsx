dd355f66c51eb730fedd4a1a2696ea10f5322ce2
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-08-31 22:51:54 +0600

    bistro boss setup tailwind daisyui react router dom awesome react carousel and basic home main routes navbar banner footer

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import Home from '../Components/Home';
+import Main from './../Main/Main';
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+    ]
+  },
+]);
