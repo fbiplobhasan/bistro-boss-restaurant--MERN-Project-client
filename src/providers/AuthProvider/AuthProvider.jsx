ab77c06bd4eeecdae054c81e859f68a75abac45b
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-09-08 20:46:27 +0600

    add auth validation

--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -75,20 +75,23 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       if (currentUser) {
-        // get token and store client side
+        // get token and store client
         const userInfo = { email: currentUser.email };
         axiosPublic.post("/jwt", userInfo).then((res) => {
           if (res.data.token) {
             localStorage.setItem("access-token", res.data.token);
+            setLoading(false);
           }
         });
       } else {
-        // TODO: remove token (if token stored in the client side: Local  storage,caching,in memory)
+        // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
         localStorage.removeItem("access-token");
+        setLoading(false);
       }
-      setLoading(false);
     });
-    return () => unsubscribe();
+    return () => {
+      return unsubscribe();
+    };
   }, [axiosPublic]);
 
   const authInfo = {
