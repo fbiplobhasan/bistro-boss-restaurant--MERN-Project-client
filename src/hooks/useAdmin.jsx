cc0b4a20e0ee88ade4f783043dd58a367928f850
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-09-09 11:31:23 +0600

    add more

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,18 +4,19 @@ import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 
 const useAdmin = () => {
-  const { user } = useAuth();
-  const axiosSecure = useAxiosSecure();
-
-  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
-    queryKey: [user?.email, "isAdmin"],
-    queryFn: async () => {
-      const res = await axiosSecure.get(`/users/admin/${user.email}`);
-    //   console.log(res.data);
-      return res.data?.admin;
-    },
-  });
-  return [isAdmin,isAdminLoading];
+    const { user, loading } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        enabled: !loading,
+        queryFn: async () => {
+            console.log('asking or checking is admin', user)
+            const res = await axiosSecure.get(`/users/admin/${user.email}`);
+            // console.log(res.data);
+            return res.data?.admin;
+        }
+    })
+    return [isAdmin, isAdminLoading]
 };
 
 export default useAdmin;
